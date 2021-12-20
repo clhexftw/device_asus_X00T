a2349f257e6b95aca3001fee3fa3d8b38ae34b2f
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-20 11:13:39 +0700

    X00T: power: Add custom mode extension for handling app launch
    Currently, we're not using CAF's BoostFramework due to having a pure
    AOSP based source, thus qcom perf hinting are not correctly applied.
    This extension will handle the first app launch hinting through
    device-specific mode override, utilizing existing libqti-perfd hinting.
    So any further configuration should be happens inside app launch (V1)
    config inside perfboostsconfig.xml
    
    Reference: vendor/qcom/opensource/power/power-6150.c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,6 +119,7 @@ TARGET_BOARD_PLATFORM := sdm660
 TARGET_USES_INTERACTION_BOOST := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 TARGET_POWERHAL_BOOST_EXT := $(DEVICE_PATH)/power/boost-ext.cpp
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/mode-ext.cpp
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
