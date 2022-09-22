a6f4509f78e0e6fee07dd2773fa3279cb7ce1984
clhex <100755123+clhexftw@users.noreply.github.com>
2022-09-22 07:07:24 -0400

    fix derp

--- arrow_X00T.mk
+++ arrow_X00T.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/common/config/common.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
